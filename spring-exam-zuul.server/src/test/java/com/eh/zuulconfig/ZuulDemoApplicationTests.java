f670e7273d91d394d9e7a391b4ed4083cbd53a88
MD EMRAN HOSSAIN <emran@medisys.com.sa>
2020-10-06 23:42:13 +0600

    Init Commit

---
+++ spring-exam-zuul.server/src/test/java/com/eh/zuulconfig/ZuulDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.eh.zuulconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
