5115e852300a6878f1c555da7ecf6ebbdabf32db
MD EMRAN HOSSAIN <emran@medisys.com.sa>
2020-10-13 22:53:13 +0600

    Modify column name in query

--- spring-jdbc-web.api/src/main/java/com/eh/webapi/util/SQLProperty.java
+++ spring-jdbc-web.api/src/main/java/com/eh/webapi/util/SQLProperty.java
@@ -10,6 +10,6 @@ public class SQLProperty {
 
     public static final String ALL_NATIONALITY       = "SELECT * FROM nationality";
     public static final String NATIONALIRY_BY_CODE   = "SELECT * FROM nationality WHERE nationality_code =?";
-    public static final String CREATE_NATIONALITY    = "INSERT INTO nationality(nationality_code, entry_date, entry_user, lang1_name, lang2_name, short_name) VALUES ( ?, ?, ?, ?, ?, ?)";
-    public static final String UPDATE_NATIONALITY    = "UPDATE nationality SET update_date = ?, update_user = ?, lang1_name = ?, lang2_name = ?, short_name = ? WHERE nationality_code = ?";
+    public static final String CREATE_NATIONALITY    = "INSERT INTO nationality(nationality_code, entry_date, entry_user, name_native, name_global, short_name) VALUES ( ?, ?, ?, ?, ?, ?)";
+    public static final String UPDATE_NATIONALITY    = "UPDATE nationality SET update_date = ?, update_user = ?, name_native = ?, name_global = ?, short_name = ? WHERE nationality_code = ?";
 }
